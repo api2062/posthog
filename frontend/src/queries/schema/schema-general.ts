f27477dd61e5db0463b83e5becbf1b050ddc5405
Rafa Audibert <rafaeelaudibert@gmail.com>
2026-03-03 17:21:03 -0300

    fix: Remove `deprecated` config
    This doesn't make sense, let's let people use RAK if they prefer

--- frontend/src/queries/schema/schema-general.ts
+++ frontend/src/queries/schema/schema-general.ts
@@ -4782,7 +4782,6 @@ export interface SourceFieldSelectConfigOption {
     label: string
     value: string
     fields?: SourceFieldConfig[]
-    deprecated?: boolean
 }
 
 export interface SourceFieldSelectConfig {
