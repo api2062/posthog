ef00b3b9fcf43d590eb6dc2d30f4a61ebb76ea3d
Vojta Bartos <vojtech.b@posthog.com>
2026-03-09 17:47:28 +0100

    fix(action): formatting issue

--- posthog/api/action.py
+++ posthog/api/action.py
@@ -58,7 +58,6 @@ class _ActionStepPropertiesField(serializers.ListField):
     pass
 
 
-
 class ActionStepJSONSerializer(serializers.Serializer):
     event = serializers.CharField(
         required=False,
